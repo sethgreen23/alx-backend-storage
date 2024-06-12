9c2f5b5ebd701484f3298412926489007927e0f0
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-12 19:39:40 +0100

    task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,14 @@
+-- create table users with id, email, name and country
+
+CREATE TABLE IF NOT EXISTS users (
+	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
+
+
+
+
+
+
