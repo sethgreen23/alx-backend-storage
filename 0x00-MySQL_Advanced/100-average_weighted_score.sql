998090844f1d41cde48304d676b677f685eb397d
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-14 00:54:23 +0100

    task 100

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,41 @@
+-- Write a SQL script that creates a stored procedure 
+-- ComputeAverageWeightedScoreForUser that computes and
+-- store the average weighted score for a student.
+
+-- Computes and store the average score for a student
+
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser
+(IN user_id INT)
+BEGIN
+	-- declare variables
+	DECLARE weighted_average FLOAT DEFAULT 0;
+	DECLARE hole_score INT DEFAULT 0;
+	DECLARE sum_weights INT DEFAULT 0;
+
+	-- compute weighted average
+	SELECT SUM(projects.weight * corrections.score) INTO hole_score 
+	FROM projects INNER JOIN corrections 
+	WHERE projects.id = corrections.project_id 
+	AND corrections.user_id = user_id;
+	
+	-- compute sum of weights
+	SELECT SUM(projects.weight) INTO sum_weights 
+	FROM projects INNER JOIN corrections 
+	WHERE projects.id = corrections.project_id 
+	AND corrections.user_id = user_id;
+	
+	-- compute weighted average
+	IF sum_weights  = 0 THEN
+		SET weighted_average = 0;
+	ELSE
+		SET weighted_average = hole_score / sum_weights;
+	END IF;
+
+	-- update table
+	UPDATE users 
+	SET average_score = weighted_average
+	WHERE users.id = user_id;
+END $$
+DELIMITER ;
+
