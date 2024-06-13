4ac5bc09a8750b817aca3e443eefd23e4ea25cc8
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-13 23:22:43 +0100

    task 11

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -3,6 +3,6 @@
 -- more than 1 month.
 
 CREATE VIEW need_meeting AS
-SELECT * FROM students
+SELECT name FROM students
 WHERE score < 80 AND
 (last_meeting IS NULL OR last_meeting < DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH)); 
