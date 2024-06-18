03057feb753b3b3939a8d99e36f6fd10a56d5233
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-18 18:20:46 +0100

    task 0

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,12 +2,11 @@
 """list all documents in a collection"""
 
 
-if __name__ == "__main__":
-	from pymongo import MongoClient
+from pymongo import MongoClient
 
 
 
-	def list_all(mongo_collection):
-		"""Return all documents in a collection"""
-		documents = mongo_collection.find()
-		return documents if documents is not None else []
+def list_all(mongo_collection):
+	"""Return all documents in a collection"""
+	documents = mongo_collection.find()
+	return documents if documents is not None else []
