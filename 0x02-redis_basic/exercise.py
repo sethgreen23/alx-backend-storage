1bb740b9d8ecbec57fe179b2a5765cd234c021d5
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-20 00:28:29 +0100

    task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Basic Redis operations
+"""
+import redis
+from uuid import uuid4
+from typing import Callable, Union
+from functools import wraps
+
+
+class Cache:
+    """ Cache class"""
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int]) -> str:
+        """ Store method """
+        key = str(uuid4())
+        data = data.decode('utf-8')
+        self._redis.set(key, data)
+        return key
